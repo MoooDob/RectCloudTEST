69db6bb3f6127ccb47a5d46f8326ae9861f40e32
Marc O. Ruedel <mor@uni-bremen.de>
2019-12-17 09:13:07 +0100

    Before converting to Spiral of Theodorus

--- RectangleTEST/src/Application/SpiralTEST.java
+++ RectangleTEST/src/Application/SpiralTEST.java
@@ -7,12 +7,15 @@ import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.Label;
 import javafx.scene.input.KeyCode;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 import javafx.scene.paint.Color;
-import javafx.scene.shape.Circle;
-import javafx.scene.shape.Line;
+import javafx.scene.shape.ArcType;
 import javafx.scene.shape.Rectangle;
+import javafx.scene.shape.StrokeLineCap;
+import javafx.scene.shape.StrokeLineJoin;
 
 public class SpiralTEST extends Application {
 
@@ -36,35 +39,52 @@ public class SpiralTEST extends Application {
 	*
 	*/
 	@Override
-	public void start(Stage stage) {			
-
-		// Creating a Group object
-		Group root = new Group();
+	public void start(Stage stage) {	
 		
-//		for (Rectangle r : rectangles) {
-//			root.getChildren().add(r);
-//		}
+        Label label = new Label("Press N to create new rects.");
+        
+    	// Creating a Group object
+		Group rectGroup = new Group();
+
+        BorderPane root = new BorderPane(rectGroup);
+        root.setBottom(label);
+       
 
 		// for debugging
 		Canvas canvas = new Canvas(scene_width, scene_height);
-		GraphicsContext gc = canvas.getGraphicsContext2D();
-		
-		// for visual Debugging
-		root.getChildren().add(canvas);
+		GraphicsContext textRectGC = canvas.getGraphicsContext2D();
+		textRectGC.setStroke(Color.GREY);
+		textRectGC.setLineWidth(1);
+//		textRectGC.setLineCap(StrokeLineCap.BUTT);
+//		textRectGC.setLineJoin(StrokeLineJoin.BEVEL);
+//		textRectGC.setLineDashes(new double[] { 7, 7 });
+		rectGroup.getChildren().add(canvas); 
+
+		// for debugging too
+		Canvas spiralCanvas = new Canvas(scene_width, scene_height);
+		GraphicsContext spiralGC = spiralCanvas.getGraphicsContext2D();
+		spiralGC.setStroke(Color.RED);
+		spiralGC.setFill(Color.RED);
+		rectGroup.getChildren().add(spiralCanvas);
 
 		// Creating a scene object
 		Scene scene = new Scene(root, scene_width, scene_height);
 		scene.setOnKeyPressed(e -> {
 		    if (e.getCode() == KeyCode.N) {
-		    	Rectangle r = createAndSetNewRect(gc);
+		    	spiralGC.clearRect(0, 0, scene_width, scene_height);
+		    	textRectGC.clearRect(0, 0, scene_width, scene_height);
+		    	Rectangle r = createAndSetNewRect(textRectGC, spiralGC);
 				if (r != null){
-					root.getChildren().add(r);
+					rectGroup.getChildren().add(r);
 				}
 		    }
 		});
 
+		// Adding Title to the stage
+        stage.setTitle("Spiral Rectangle Cloud");
+
 		// Adding scene to the stage
-		stage.setScene(scene);
+		stage.setScene(scene);		
 
 		// Displaying the contents of the stage
 		stage.show();
@@ -73,27 +93,43 @@ public class SpiralTEST extends Application {
 	
 	
 	
-	private Rectangle createAndSetNewRect(GraphicsContext gc) {
-		double rectWidth = randomizer.nextDouble() * maxRectWidth;
-		double rectHeight = randomizer.nextDouble() * maxRectHeight;
-
-
-
-		Rectangle newRect = checkRectCollision(
-				scene_width / 2, scene_height / 2, /*new location*/ 
-				100, /* max Radius */
-				40, /* Segments */
-				2.0, /* Spinrate */
-				rectWidth, /* rectWidth */
-				rectHeight, /* rectHeight */
-				gc
-		);
+	private Rectangle createAndSetNewRect(GraphicsContext textRectGC, GraphicsContext spiralGC) {
 		
-		if (newRect != null) {
-			newRect.setStroke(Color.AQUAMARINE);
-			newRect.setFill(Color.TRANSPARENT);
-			rectangles[rectCounter] = newRect;
-			rectCounter++;
+		System.out.println("");
+		Rectangle newRect = null;
+		
+		if (rectCounter < numOfRectangles) {
+		
+			// create random width and height in the defined limits 
+			double rectWidth = Math.floor(randomizer.nextDouble() * maxRectWidth);
+			double rectHeight = Math.floor(randomizer.nextDouble() * maxRectHeight);
+	
+			newRect = checkRectCollision(
+					scene_width / 2, scene_height / 2, /*new location*/ 
+					500, /* max Radius */
+					0, // 40.0, /* Segments or -> Segments==0 && deltaRadius > 0 */
+					10000.0, // 2.0, /* Spinrate */
+					1, /* deltaRadius or -> Segments */
+					rectWidth, /* rectWidth */
+					rectHeight, /* rectHeight */
+					textRectGC, 
+					spiralGC
+			);
+			
+			if (newRect != null) {
+				newRect.setStroke(Color.BLACK);
+				newRect.setFill(Color.TRANSPARENT);
+				rectangles[rectCounter] = newRect;
+				rectCounter++;
+				System.out.println(String.format("rect @ x= %.0f y= %.0f width= %.0f height= %.0f added.",
+						newRect.getX(),
+						newRect.getY(),
+						newRect.getWidth(),
+						newRect.getHeight()
+				));
+			}
+		} else {
+			System.out.println("max num of rects.");
 		}
 		
 		return newRect;
@@ -109,18 +145,44 @@ public class SpiralTEST extends Application {
 				new Rectangle(x - rectHeight / 2, y - rectWidth / 2, rectWidth, rectHeight), // Upright rect
 				new Rectangle(x - rectWidth / 2, y - rectHeight / 2, rectHeight, rectWidth)  // 90 degree rotated rect
 				};
+		
+		System.out.println("check new spiral coordinates x= " + x + ", y= " + y + " with width= " + rectWidth + ", height= " + rectHeight);
 
+		System.out.println("check collision of new rect against all " + rectCounter + " already positioned rects. ");
 		Rectangle theRect = testRects[0];
 		for (Rectangle r : rectangles) {
 			
 			if (r == null) return theRect;
 			
+			System.out.println(String.format("check rect @ x= %.0f y= %.0f width= %.0f height= %.0f",
+					r.getX(),
+					r.getY(),
+					r.getWidth(),
+					r.getHeight()
+			));
+			
 			for (Rectangle testRect : testRects) {
-				theRect = 
-						r.intersects(testRect.getBoundsInLocal()) ? null : testRect;
-				if (theRect != null) {
-					return theRect;
-				}				
+				
+				System.out.println(String.format("  against rect @ x= %.0f y= %.0f width= %.0f height= %.0f",
+						testRect.getX(),
+						testRect.getY(),
+						testRect.getWidth(),
+						testRect.getHeight()
+				));
+
+
+				// getBoundsInLocal is only correct if and only if both objects are in the same coordinate system. 
+				// if one object is for instance part of another group, the coordinate system have to be transformed
+				// furthermore this function tests only the AABB (axis aligned bounding box) of both objects, 
+				// so rotated will be approximated with its AABB
+//				theRect = 
+//						r.getBoundsInParent().intersects(testRect.getBoundsInLocal()) ? null : testRect;
+//				if (theRect != null) {
+//					System.out.println("no collision...");
+//					return theRect;
+//				} else {
+//					System.out.println("collision!");
+//				}
 			}
 		}
 
@@ -131,45 +193,76 @@ public class SpiralTEST extends Application {
 	
 	
 	// according to https://stackoverflow.com/q/48492980
-	public Rectangle checkRectCollision(double x0, double y0, double maxRadius, int numSegments, double spinRate,
-			double rectWidth, double rectHeight, GraphicsContext gc) {
-		double radius = 0;
+	/**
+	 * @param x0
+	 * @param y0
+	 * @param maxRadius
+	 * @param numSegments
+	 * @param spinRate
+	 * @param deltaR
+	 * @param rectWidth
+	 * @param rectHeight
+	 * @param textRectGC
+	 * @param spiralGC
+	 * @return
+	 */
+	public Rectangle checkRectCollision(double x0, double y0, double maxRadius, double numSegments, double spinRate, double deltaR,
+			double rectWidth, double rectHeight, GraphicsContext textRectGC, GraphicsContext spiralGC) {
+		double radius = 0.00001;
 		double x = x0;
 		double y = y0;
-		double deltaR = maxRadius / numSegments;
+		if (numSegments > 0) {
+			deltaR = maxRadius / numSegments;
+		} else {
+			numSegments = maxRadius / deltaR;
+		}
+	
 
-		int step = 0;
-		Rectangle newRect; 
-		while ((newRect = massCheckCollisonAABB(x, y, rectWidth, rectHeight)) == null) {
+		int step = 1;
+		Rectangle newRect = null; 
+		while (
+//				(newRect = massCheckCollisonAABB(x, y, rectWidth, rectHeight)) == null && 
+				radius < maxRadius
+				) {
 			double x1 = x;
 			double y1 = y;
-			radius += deltaR;
-			x = x0 + radius * Math.cos((2 * Math.PI * step / numSegments) * spinRate);
-			y = y0 + radius * Math.sin((2 * Math.PI * step / numSegments) * spinRate);
-			gc.setStroke(Color.LIME);
-			gc.strokeLine(x, y, x1, y1);
+//			radius += deltaR;
+			radius += deltaR * step / (radius * 10);
+//			x = x0 + radius * Math.cos((2 * Math.PI * step / numSegments) * spinRate);
+//			y = y0 + radius * Math.sin((2 * Math.PI * step / numSegments) * spinRate);
+			x = x0 + radius * Math.cos((2 * Math.PI * step / (radius * 10)));
+			y = y0 + radius * Math.sin((2 * Math.PI * step / (radius * 10)));
+			spiralGC.strokeLine(x, y, x1, y1);
 
-			gc.setStroke(Color.BLUE);
-            gc.strokeRect(x - rectHeight /2, y - rectWidth / 2, rectHeight, rectWidth);
-            gc.strokeRect(x - rectWidth /2, y - rectHeight / 2, rectWidth, rectHeight);
+			spiralGC.fillOval(x-5/2, y-5/2, 5, 5);
 
-//		            gc.strokeRect(x, y, rectWidth, rectHeight);
-//		            gc.strokeRect(x-rectWidth, y, rectWidth, rectHeight);
-//		            gc.strokeRect(x, y-rectHeight, rectWidth, rectHeight);
-//		            gc.strokeRect(x-rectWidth, y-rectHeight, rectWidth, rectHeight);
-//		            
-//		            gc.strokeRect(x, y, rectHeight, rectWidth);
-//		            gc.strokeRect(x-rectHeight, y, rectHeight, rectWidth);
-//		            gc.strokeRect(x, y-rectHeight, rectHeight, rectWidth);
-//		            gc.strokeRect(x-rectHeight, y-rectWidth, rectHeight, rectWidth);
+//			  textRectGC.setStroke(Color.GREY);
+//            textRectGC.strokeRect(x - rectHeight /2, y - rectWidth / 2, rectHeight, rectWidth);
+//            textRectGC.strokeRect(x - rectWidth /2, y - rectHeight / 2, rectWidth, rectHeight);
 
-			System.out.println("x=" + x + " y=" + y);
+//            textRectGC.strokeRect(x, y, rectWidth, rectHeight);
+//            textRectGC.strokeRect(x-rectWidth, y, rectWidth, rectHeight);
+//            textRectGC.strokeRect(x, y-rectHeight, rectWidth, rectHeight);
+//            textRectGC.strokeRect(x-rectWidth, y-rectHeight, rectWidth, rectHeight);
+//	            
+//            textRectGC.strokeRect(x, y, rectHeight, rectWidth);
+//            textRectGC.strokeRect(x-rectHeight, y, rectHeight, rectWidth);
+//            textRectGC.strokeRect(x, y-rectHeight, rectHeight, rectWidth);
+//            textRectGC.strokeRect(x-rectHeight, y-rectWidth, rectHeight, rectWidth);
+
+//			System.out.println(String.format("radius0 %.2f x= %.0f y= %.0f",
+//					radius,
+//					x,
+//					y
+//			));
+			
+			step++;
 
 		}
 
-		gc.setStroke(Color.AQUAMARINE);
+		textRectGC.setStroke(Color.AQUAMARINE);
 		if (newRect != null) {
-			gc.strokeRect(newRect.getX(), newRect.getY(), newRect.getWidth(), newRect.getHeight());
+			textRectGC.strokeRect(newRect.getX(), newRect.getY(), newRect.getWidth(), newRect.getHeight());
 		}
 		
 		return newRect;
